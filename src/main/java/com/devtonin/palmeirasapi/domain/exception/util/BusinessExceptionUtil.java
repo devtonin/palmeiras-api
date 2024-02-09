fd4517d7076b1b7dd2bfac8629a78635a70fecb9
antonioamaral <antonio.amaral@sensedia.com>
2024-02-09 16:37:55 -0300

    [feat] added BusinessExceptionUtil

---
+++ src/main/java/com/devtonin/palmeirasapi/domain/exception/util/BusinessExceptionUtil.java
@@ -0,0 +1,48 @@
+package com.devtonin.palmeirasapi.domain.exception.util;
+
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.http.HttpStatus;
+import com.devtonin.palmeirasapi.domain.exception.BusinessException;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class BusinessExceptionUtil {
+
+	public static void throwsIf(boolean condition, @NonNull MessageException... message) throws BusinessException {
+
+		if (condition) {
+			throwsException(message);
+		}
+	}
+
+    public static void throwsIf(boolean condition, HttpStatus httpStatusCode, @NonNull MessageException... message) throws BusinessException {
+
+		if (condition) {
+			throwsException(httpStatusCode, message);
+		}
+	}
+
+	public static void throwsException(@NonNull MessageException... message) throws BusinessException {
+
+		throw new BusinessException(Arrays.asList(message));
+	}
+
+	public static void throwsException(@NonNull List<MessageException> messages) throws BusinessException {
+
+		throw new BusinessException(messages);
+	}
+	
+	public static void throwsException(HttpStatus httpStatusCode, @NonNull MessageException... message) throws BusinessException {
+
+		throw new BusinessException(Arrays.asList(message), httpStatusCode);
+	}
+
+	public static void throwsException(HttpStatus httpStatusCode, @NonNull List<MessageException> messages) throws BusinessException {
+
+		throw new BusinessException(messages, httpStatusCode);
+	}
+
+}
